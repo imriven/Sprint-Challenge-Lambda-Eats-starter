9bd1e75ae4bbcda25186f7a676f65c56046e0d82
Christina Harris <christinathedesigner@outlook.com>
2020-03-27 08:29:54 -0700

    Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Route, NavLink } from "react-router-dom";
+import Home from "./components/home"
+import Pizza from "./components/pizza"
+import Success from "./components/success"
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <Route exact path="/ Pizza"></Route>
+    <nav>
+      <h3>Lambda Eats</h3>
+    <div>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to={Pizza} >Order</NavLink>
+    </div>
+    </nav>
+
+    {/* Routes / Paths */}
+    <Route exact path="/"><Home /></Route>
+    <Route>< Pizza /> </Route>
+
     </>
   );
 };
